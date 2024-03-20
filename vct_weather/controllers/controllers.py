e79e44d62f4a67c524715f0c3c7c64b94adc034c
tienle1506 <tienlee156@gmail.com>
2024-03-20 08:43:52 +0700

    update weather 2

--- vct_weather/controllers/controllers.py
+++ vct_weather/controllers/controllers.py
@@ -5,25 +5,6 @@ import json
 class WeatherController(http.Controller):
     @http.route('/weather', type='http', auth='public', website=True, methods=['GET'], csrf=False)
     def index(self, **kwargs):
-        # print('Done')
-        # day = int(kwargs.get('day', 1))
-        # weather_records = http.request.env['weather'].search([], limit=day)
-        #
-        # weather_data = []
-        # for record in weather_records:
-        #     weather_data.append({
-        #         'day_item': record.day_item,
-        #         'date_item': record.date_item,
-        #         'temp_hi': record.temp_hi,
-        #         'temp_lo': record.temp_lo,
-        #         'no_wrap1': record.no_wrap1,
-        #         'no_wrap2': record.no_wrap2,
-        #         'precip_content': record.precip_content,
-        #     })
-        # # response = json.dumps(weather_data)
-        # # print(response)
-        # # return response
-        # print(weather_data)
         return http.request.render('vct_weather.weathers', {
         })
 
